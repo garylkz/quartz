4f9ad1cc1645b4c38d43ea243d7c5b2c2e74aad2
cicadoves <cicadoves@gmail.com>
2021-06-12 08:35:08 +0200

    in theory

---
+++ qct-intel.py
@@ -0,0 +1,50 @@
+import os, requests
+from datetime import date
+from discord.ext import commands
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+open('creds.json', 'w').write(requests.get(os.environ['link']).text)
+scope = [
+        'https://spreadsheets.google.com/feeds',
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets().values()
+os.remove('creds.json')
+
+def sheet_get(range):
+    return sheet.get(
+            spreadsheetId=qct,
+            range=range).execute().get('values', [])
+
+qct = '1JL8Vfyj4uRVx6atS5njJxL03dpKFkgBu74u-h0kTNSo'
+cardlist = "Card List!A:Z"
+collist = "Collection!A:B"
+getcardlist = sheet_get(cardlist)
+
+"""
+card = [
+        album,
+        collection,
+        name,
+        rarity,
+        type,
+        cost,
+        power,
+        ppe,
+        ability,
+        model,
+        date]
+"""
+
+@commands.command()
+async def whatis(ctx, *, kwargs):
+    if any(kwargs in i for i in getcardlist):
+        await ctx.send(i)
+
+def setup(bot):
+    bot.add_command(whatis)
+
