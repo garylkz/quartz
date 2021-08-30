116e0a9ad982bd4e701dc8e6aad243a0c5e8fe3b
Gary <gary.github@gmail.com>
2021-08-30 12:26:19 +0800

    Permission check, legacy staging, image crawling

--- embed.py
+++ embed.py
@@ -1,8 +1,8 @@
 from datetime import date
 import os
-from random import random
 import re
 
+from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -28,8 +28,12 @@ LOGS = 'Changelog!A:B'
 SHEET = '1JL8Vfyj4uRVx6atS5njJxL03dpKFkgBu74u-h0kTNSo'
 USER = 'USER_ENTERED'
 
-append = lambda r, b : sheet.append(spreadsheetId=SHEET, range=r, body=b, valueInputOption=USER).execute()
+append = lambda r, b, i=SHEET : sheet.append(spreadsheetId=i, range=r, body=b, valueInputOption=USER).execute()
 get = lambda r : sheet.get(spreadsheetId=SHEET, range=r).execute().get('values', [])
+
+
+IDS = []
+appends = lambda r, b, i : [append(r, b, i) for i in IDS]
 
 DEBUG = False
 
@@ -47,15 +51,20 @@ def card_get(embed) -> list:
     ability = embed.fields[3].name
     description = embed.fields[3].value
     log = str(date.today())
-    card = [album, collection, name, rarity, status, cost, power, ppe, ability, description, model, log]
+    url = embed.image.url
+    card = [album, collection, name, rarity, status, cost, power, ppe, ability, description, model, log, url]
     if DEBUG:
         print(card)
     return card
 
 async def on_embed(msg):
-    # Only respond to @CUE#3444
+    # @CUE#3444 check
     if msg.author.id != 739553550224588810:
         return
+    # Card not found check
+    if re.search('^Search text ".*" not found$', msg.content):
+      await msg.channel.send('Potential card removal')
+      # TODO: move card to Legacy
     # Embed check
     for embed in msg.embeds:
         if DEBUG:
@@ -63,23 +72,33 @@ async def on_embed(msg):
         card = _, c, n, r, *_ = card_get(embed)
         body = {'values': [card]}
         today = str(date.today())
-        if any(n in i for i in get(CNAMES)):
-            await msg.channel.send('Data exist.')
-        else:
-            append(CARDS, body)
-            body['values'] = [[n, today]]
-            append(LOGS, body)
-            await msg.channel.send('Data added.')
-            await msg.delete()
-        if 'Fusion' in r:
-            body['values'] = [[n]]
-            append('Fusion!A:A', body)
-            await msg.channel.send('Fusion detected.')
-        if not any(c in i[0] for i in get(SNAMES)):
-            p = re.search('(^[A-Z]+)[0-9]+$', card[10]).group(1)
-            body['values'] = [[c, '', p, today]]
-            append(SNAMES, body)
-            await msg.channel.send('New collection detected.')
+        try:
+            cards = get(CARDS)
+            names = [i[2] for i in cards]
+            if n in names:
+                i = names.index(n)
+                if card == cards[i]:
+                    await msg.channel.send(f'Data exist. (row {i})')
+                else:
+                    await msg.channel.send('>existing data outdated\n>move it to legacy section\n>replace')
+                    # TODO: move card to Legacy
+            else:
+                append(CARDS, body)
+                body['values'] = [[n, today]]
+                append(LOGS, body)
+                await msg.channel.send('Data added.')
+                await msg.delete()
+            if 'Fusion' in r:
+                body['values'] = [[n]]
+                append('Fusion!A:A', body)
+                await msg.channel.send('Fusion detected.')
+            if not any(c in i[0] for i in get(SNAMES)):
+                p = re.search('(^[A-Z]+)[0-9]+$', card[10]).group(1)
+                body['values'] = [[c, '', p, today]]
+                append(SNAMES, body)
+                await msg.channel.send('New collection detected.')
+        except HttpError:
+            await msg.channel.send("But I don't have permission to edit the sheet!")
 
 def setup(bot):
     bot.add_listener(on_embed, 'on_message')
