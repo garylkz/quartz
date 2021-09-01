49c0d0665dbd962b7b0680b6fa91b9655548d759
Gary <gary.github@gmail.com>
2021-09-01 15:39:03 +0800

    Smart Album detection

--- embed.py
+++ embed.py
@@ -2,7 +2,7 @@ import asyncio
 from datetime import date
 import os
 import re
-from typing import Awaitable
+from typing import Awaitable, List
 
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
@@ -48,17 +48,28 @@ def debug_log(chn) -> Awaitable:
 
 
 # Extract card info from embed
-def extract(embed, r: int, date: str) -> list:
-    album = f'=VLOOKUP(B{r}, Collection!A:B, 2, false)'
+def extract(embed, date: str) -> list:
     collection = embed.footer.text
     model, name = embed.title.split(' ', 1)
+    album = (
+        'Art and Culture' if model.startswith(('AC')) else
+        'History' if model.startswith(('E', 'MYHI')) else
+        'Life on Land' if model.startswith(('L', 'MYLO')) else
+        'Oceans' if model.startswith(('O', 'MYSE')) else
+        'Paleontology' if model.startswith(('P', 'MYPA')) else
+        'Science' if model.startswith(('MYSC')) else
+        'Space' if model.startswith(('MYSP')) else ''
+    )
     value = embed.fields[0].value.split()
-    status, rarity = value if 'Limited' in value else ('Standard', value[0])
+    status, rarity = value if 'Limited' in value else ('', value[0])
     cost = embed.fields[1].value
-    power = embed.fields[2].value 
+    power = embed.fields[2].value
     ppe = '∞' if cost == '0' else str(int(power)//int(cost))
-    ability = embed.fields[3].name
-    description = embed.fields[3].value
+    if 'Buffed by' in embed.fields[3].name:
+        ability = description = ''
+    else:
+        ability = embed.fields[3].name
+        description = embed.fields[3].value
     url = embed.image.url
     card = [album, collection, name, status, rarity, cost, power, ppe, ability, description, model, date, url]
     return card
@@ -70,6 +81,7 @@ async def on_embed(msg):
         return
     # Set debug channel
     log = debug_log(msg.channel)
+    ls = []
     # Access qct sheets data
     data = sheet.values().batchGet(
         spreadsheetId=SHEET,
@@ -82,7 +94,7 @@ async def on_embed(msg):
     if notFound:
         n = notFound.group(1).lower()
         if n in ns:
-            await log('EVENT: REMOVE')
+            ls.append('1. EVENT: REMOVE')
             i = ns.index(n)
             card = ['Removed'] + cards[i]
             del cards[i]
@@ -91,53 +103,56 @@ async def on_embed(msg):
             append(LGCYS, [card])
             await msg.channel.send('Card removed.')
         else:
-            await log('IGNORE: NOT FOUND')
-        return
+            ls.append('1. IGNORE: NOT FOUND')
     # Embed check
     for embed in msg.embeds:
         # Multiple results check
-        if re.search('^Results for ".+":.*', embed.title):
-            await log('IGNORE: RESULTS')
-            return
-        elif 'CUEbot Help' in embed.title:
-            await log('IGNORE: HELP')
-            return
-        # Extract info 
-        today = str(date.today())
-        card = _, s, n, r, *_ = extract(embed, len(cards)+1, today)
-        try:
-            n_ = n.lower()
-            await log('NAME IN LIST: ' + str(n_ in ns).upper())
-            if n_ in ns:
-                # Compare data
-                i = ns.index(n_)
-                await asyncio.gather(
-                    log(card, header='Latest'),
-                    log(cards[i], header='Existing')
-                )
-                if card[:11] == cards[i][:11]:
-                    await msg.channel.send(f'Data exist.')
+        if (re.search('^Results for ".+":.*', embed.title)
+                or 'CUEbot Help' in embed.title):
+            ls.append('1. IGNORE: NOT CARD')
+        # Is card check
+        elif re.search('[A-Z]+[0-9]+ .+', embed.title):
+            # Extact info
+            today = str(date.today())
+            card = _, s, n, r, *_ = extract(embed, today)
+            try:
+                n_ = n.lower()
+                ls.append('1. NAME IN LIST: ' + str(n_ in ns).upper())
+                if n_ in ns:
+                    # Compare data
+                    i = ns.index(n_)
+                    await asyncio.gather(
+                        log(card, header='Latest'),
+                        log(cards[i], header='Current')
+                    )
+                    latest = (card[:3], card[4:11])
+                    current = (cards[i][:3], cards[i][4:11])
+                    if current == latest:
+                        ls.append('2. IGNORE: IDENTICAL')
+                        await msg.channel.send(f'Data exist.')
+                    else:
+                        ls.append('2. EVENT: UPDATE')
+                        pass # TODO: Legacy
                 else:
-                    await log('EVENT: UPDATE')
-                    pass # TODO: Legacy
-            else:
-                append(CARDS, [card])
-                append('Changelog!A:B', [[n, today]])
-                await msg.channel.send('Data added.')
-            # Fusion check
-            if 'Fusion' in r:
-                append('Fusion!A:A', [[n]])
-                await msg.channel.send('Fusion detected.')
-            # Collection check
-            if not any(s in i[0] for i in cols):
-                p = re.search('(^[A-Z]+)[0-9]+$', card[10]).group(1)
-                append(CLTS, [[s, '', p, today]])
-                await msg.channel.send('Collection detected.')
-        except HttpError:
-            await msg.channel.send("No 'Editor' permission.")
-        # Debug cleanup
-        if DEBUG:
-            await msg.delete()
+                    ls.append('EVENT: APPEND')
+                    append(CARDS, [card])
+                    append('Changelog!A:B', [[n, today]])
+                    await msg.channel.send('Data added.')
+                # Fusion check
+                if 'Fusion' in r:
+                    append('Fusion!A:A', [[n]])
+                    await msg.channel.send('Fusion detected.')
+                # Collection check
+                if not any(s in i[0] for i in cols):
+                    p = re.search('(^[A-Z]+)[0-9]+$', card[10]).group(1)
+                    append(CLTS, [[s, '', p, today]])
+                    await msg.channel.send('Collection detected.')
+            except HttpError:
+                await msg.channel.send("No 'Editor' permission.")
+    # Debug cleanup
+    if DEBUG:
+        await log('\n'.join(ls))
+        await msg.delete()
 
 
 def setup(bot):
