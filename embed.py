3b260615f74f48f0fd2570d86d8dadc213a0b74a
Gary <gary.github@gmail.com>
2021-09-16 17:41:55 +0800

    Calm before storm

--- embed.py
+++ embed.py
@@ -1,5 +1,6 @@
 import asyncio
 from datetime import date
+import json
 import os
 import re
 from typing import Awaitable, List
@@ -9,22 +10,20 @@ from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Authentication
-CREDS = 'creds.json'
-open(CREDS, 'w').write(os.environ['CREDS'])
 scope = [
         'https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/spreadsheets',
         'https://www.googleapis.com/auth/drive.file',
         'https://www.googleapis.com/auth/drive'
 ]
-creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS, scope)
-os.remove(CREDS)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(os.environ['CREDS']), scope)
 
 # Goole Sheets service
 service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
 
 SHEET = '1JL8Vfyj4uRVx6atS5njJxL03dpKFkgBu74u-h0kTNSo'
+# 1jxPQD_14rdpJ_kKzWgphcuzjsTYdCpBePbg3mmtxesE
 USER = 'USER_ENTERED'
 
 append = lambda r, b, : sheet.values().append(spreadsheetId=SHEET, range=r, body={'values': b}, valueInputOption=USER).execute()
@@ -78,8 +77,7 @@ def extract(embed) -> list:
     else:
         ability = embed.fields[3].name
         description = embed.fields[3].value
-    card = [album, collection, name, status, rarity, cost, power, ppe, ability, description, model]
-    return card
+    return [album, collection, name, status, rarity, cost, power, ppe, ability, description, model]
 
 
 async def on_embed(msg):
@@ -113,19 +111,15 @@ async def on_embed(msg):
                 ls.append('1. IGNORE: HELP')
             # Is card check
             elif re.search('[A-Z]+[0-9]+ .+', embed.title):
-                today = str(date.today())
                 card = a, c, n, r, *_ = extract(embed)
+                today = str(date.today())
+                # Name in list check
                 n_ = n.lower()
                 inList = n_ in ns
                 ls.append('1. NAME IN LIST: ' + str(inList).upper())
                 if inList:
-                    # Partial compare
+                    # Update check
                     i = ns.index(n_)
-                    if DEBUG: 
-                        await asyncio.gather(
-                            log(card, header='Latest'),
-                            log(cards[i], header='Current')
-                        )
                     bot = card[:3] + card[4:11]
                     qct = cards[i][:3] + cards[i][4:11]
                     await asyncio.gather(
@@ -133,6 +127,7 @@ async def on_embed(msg):
                         log(qct, header='Current')
                     )
                     if bot == qct:
+                        # Url special
                         if len(qct) == 13:
                             ls.append('2. IGNORE: IDENTICAL')
                             await msg.channel.send('Data exist.')
@@ -141,18 +136,19 @@ async def on_embed(msg):
                             cards[i].append(embed.image.url)
                             update(CARDS, cards)
                             await msg.channel.send('Easter eggs added.')
-                    else:
+                    else: # Update is needed
                         ls.append('2. UPDATE: CARD')
                         lgcy = ['Updated'] + cards[i]
                         append(LGCYS, [lgcy])
-                        cards[i] = card[:3] + [cards[i][3]] + card[4:11] + [cards[i][11]] + [embed.image.url]
+                        cards[i] = card[:3] + [cards[i][3]] + card[4:11] + [cards[i][11], embed.image.url]
                         update(CARDS, cards)
-                        await msg.channel.send('Card updated')
+                        await msg.channel.send('Card updated.')
                 else:
-                    ls.append('EVENT: APPEND')
-                    append(CARDS, [card])
+                    # New card detected
+                    ls.append('2. APPEND: CARD')
+                    append(CARDS, [card + [today]])
                     append('Changelog!A:B', [[n, today]])
-                    await msg.channel.send('Data added.')
+                    await msg.channel.send('New card detected.')
                 # Fusion check
                 if 'Fusion' in r:
                     append('Fusion!A:A', [[n]])
