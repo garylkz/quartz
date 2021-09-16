8d807aad0f96bf1d87857060ea5d3e1d78ad3bc5
Gary <gary.github@gmail.com>
2021-09-16 17:57:41 +0800

    Test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = open('requirements.txt').read().splitlines()
 
 setuptools.setup(
         name='Project Q',
-        version='0.1',
+        version='0.1.0',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Q Discord bot.',
