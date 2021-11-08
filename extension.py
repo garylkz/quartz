3b83f14a45ca2d97af0f5e57eb976b692be22966
Gary <gary.github@gmail.com>
2021-11-08 22:28:16 +0800

    PEP PEP PEP

--- extension.py
+++ extension.py
@@ -1,5 +1,5 @@
 import asyncio, datetime, json, os, re
-from typing import Literal
+from typing import Literal, List
 
 from discord.ext import commands
 from googleapiclient import discovery
@@ -12,36 +12,57 @@ SCOPE = [
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/drive.file',
     'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/spreadsheets'
-]
+    'https://www.googleapis.com/auth/spreadsheets']
 creds = ServiceAccountCredentials.from_json_keyfile_dict(CREDS, SCOPE)
 service = discovery.build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
 
-ID = '1JL8Vfyj4uRVx6atS5njJxL03dpKFkgBu74u-h0kTNSo' # Not confidential
+ID = os.environ['ID'] if 'ID' in os.environ else ''
 CARDS = 'Card List!A:M' 
 COLS = 'Collection!A:D'
 LGCYS = 'Legacy Cards!A:AZ'
 
+sheet_get = lambda r : sheet.values().get(
+    spreadsheetId=ID, 
+    range=r).execute().get('values', [])
 
-sheet_get = lambda r : sheet.values().get(spreadsheetId=ID, range=r).execute().get('values', [])
-sheet_append = lambda r, b : sheet.values().append(spreadsheetId=ID, range=r, body={'values': [b]}, valueInputOption='USER_ENTERED').execute()
-sheet_update = lambda r, b : sheet.values().update(spreadsheetId=ID, range=r, body={'values': b}, valueInputOption='USER_ENTERED').execute()
+sheet_append = lambda r, b : sheet.values().append(
+    spreadsheetId=ID, 
+    range=r, 
+    body={'values': [b]}, 
+    valueInputOption='USER_ENTERED').execute()
+
+sheet_update = lambda r, b : sheet.values().update(
+    spreadsheetId=ID, 
+    range=r, 
+    body={'values': b}, 
+    valueInputOption='USER_ENTERED').execute()
 
 
-def card_album(model: str) -> Literal['Art and Culture', 'History', 'Life on Land', 'Oceans', 'Paleontology', 'Science', 'Space']:
-    return (
-        'Art and Culture' if model.startswith('AC') else
-        'History' if model.startswith(('E', 'MYHI', 'FHI', 'HEV')) else
-        'Life on Land' if model.startswith(('L', 'MYLO', 'FLL', 'LEV')) else
-        'Oceans' if model.startswith(('O', 'MYSE', 'FSE', 'OEV')) else
-        'Paleontology' if model.startswith(('P', 'MYPA', 'FPA', 'PEV')) else
-        'Science' if model.startswith(('MYSC', 'FSC', 'CEV')) else
-        'Space' if model.startswith(('MYSP', 'FSP', 'SEV')) else ''
-    )
+def card_album(model: str) -> Literal[
+        'Art and Culture', 'History', 'Life on Land', 
+        'Oceans', 'Paleontology', 'Science', 'Space']:
+    # TODO: Migrate
+    AC = ('AC')
+    E = ('E', 'MYHI', 'FHI', 'HEV')
+    L = ('L', 'MYLO', 'FLL', 'LEV')
+    O = ('O', 'MYSE', 'FSE', 'OEV')
+    P = ('P', 'MYPA', 'FPA', 'PEV')
+    SC = ('MYSC', 'FSC', 'CEV')
+    SP = ('MYSP', 'FSP', 'SEV') 
+
+    # old science & space both used 'S' as prefix
+    predict = ('Art and Culture' if model.startswith(AC) else
+               'History' if model.startswith(E) else
+               'Life on Land' if model.startswith(L) else
+               'Oceans' if model.startswith(O) else
+               'Paleontology' if model.startswith(P) else
+               'Science' if model.startswith(SC) else
+               'Space' if model.startswith(SP) else '')
+    return predict
 
 
-def extract(embed) -> list:
+def extract(embed) -> List[str]:
     model, name = embed.title.split(' ', 1)
     album = card_album(model)
     collection = embed.footer.text
@@ -51,28 +72,29 @@ def extract(embed) -> list:
     if not cost.isnumeric(): cost = '0'
     power = embed.fields[2].value
     if not power.isnumeric(): power = '0'
-    ppe = '∞' if cost == '0' else str(int(power)//int(cost)) # Frankenstein has 'null'
+    ppe = '∞' if cost == '0' else str(int(power)//int(cost))
     ability = description = ''
     if embed.fields[3].name != 'Buffed by':
         ability = embed.fields[3].name
         description = embed.fields[3].value
-    return [album, collection, name, status, rarity, cost, power, ppe, ability, description, model]
+    return [album, collection, name, status, rarity, cost, 
+            power, ppe, ability, description, model]
 
 
-async def check(message):
+async def check(message) -> None:
     if message.author.id != 739553550224588810: return
     for embed in message.embeds:
-        if ( # Embed but not card
-            'CUEbot Help' in embed.title or
-            re.search('^Results for "(.+)":.*$', embed.title)
-        ): return
+        if ('CUEbot Help' in embed.title or # Embed but not card
+            re.search('^Results for "(.+)":.*$', embed.title)): return
         card = extract(embed)
-        data = sheet.values().batchGet(spreadsheetId=ID, ranges=[CARDS, COLS]).execute().get('valueRanges', [])
+        data = sheet.values().batchGet(
+            spreadsheetId=ID, 
+            ranges=[CARDS, COLS]).execute().get('valueRanges', [])
         cards, subs = [i['values'] for i in data]
-        match, matches = card[10], [i[10] for i in cards] # Kinda slow
+        match, matches = card[10], [i[10] for i in cards]
         today = str(datetime.date.today())
-        # Card check
-        if match in matches:
+
+        if match in matches: # Existing card check
             i = matches.index(match)
             existing = cards[i][:11]
             card[3] = existing[3] # Hierarchy
@@ -81,71 +103,53 @@ async def check(message):
                 sheet_append(LGCYS, legacy) # Add legacy
                 cards[i] = card + [cards[i][11], embed.image.url]
                 sheet_update(CARDS, cards)
-                await message.channel.send('Update detected.')
-                await message.delete()
+                outcome = 'Update detected.'
             else: 
-                await message.channel.send('Nothing happens.')
-                if len(cards[i]) == 12: # Update image, temporary
+                if len(cards[i]) == 12: # Card check
                     cards[i].append(embed.image.url)
                     sheet_update(CARDS, cards)
-                    await message.channel.send('Oh wait, something did.')
-                    await message.delete()
-        else: # Add card
+                    outcome = 'Something happened.'
+                else: outcome = 'Nothing happens.'
+        else: # New card
             sheet_append(CARDS, card + [today, embed.image.url])
             sheet_append('Changelog!A:B', [card[2], today])
-            await asyncio.gather(
-                message.delete(),
-                message.channel.send('New card detected.')
-            )
-        # Fusion check
-        if 'Fusion' in card[3]:
+            outcome = 'New card detected.'
+
+        await asyncio.gather(
+            message.delete(),
+            message.channel.send(outcome))
+        if 'Fusion' in card[3]: # Fusion card
             sheet_append('Fusion!A:A', [card[2]])
             await message.channel.send('Fusion detected.')
-        # Collection check
-        if card[1] not in [i[0] for i in subs]:
+        if card[1] not in [i[0] for i in subs]: # New collection
             code = re.search('(^[A-Z]+)[0-9]+$', card[10]).group(1)
             sheet_append(COLS, [card[1], card[0], code, today])
             await message.channel.send('Collection detected.')
 
 
-running = True
-
-@commands.command()
-async def halt(ctx) -> None:
-    global running
-    running = False
-
-
 @commands.command('massupdate')
 async def update_all_cards(ctx) -> None:
     cards = sheet_get(CARDS)
-    while len(cards) > 0 and running:
-        card = cards.pop(0)
-        await ctx.send(f'/find {card[10]}')
+    while len(cards) > 0:
         await ctx.send(f'{len(cards)} card(s) left.')
-        await asyncio.sleep(20)
-    else:
-        await ctx.send('Finished updating.')
-        global running
-        running = True
+        card = cards.pop(0)
+        await ctx.send(f'`/find {card[10]}`')
+        await asyncio.sleep(60)
+    await ctx.send('Finished updating.')
 
 
 @commands.command('image')
 async def update_image_url(ctx) -> None:
-    cards = sheet_get(CARDS)
-    updates = []
+    cards, updates = sheet_get(CARDS), []
     for card in cards:
         if len(card) < 13:
             updates.append(card[10])
-    while len(updates) > 0 and running:
-        update = updates.pop(0)
-        await ctx.send(f'/find {update}')
+    while len(updates) > 0:
         await ctx.send(f'{len(updates)} card(s) left.')
-        await asyncio.sleep(20)
-    else:
-        await ctx.send('Finished updating.')
-        global running
-        running = True
+        update = updates.pop(0)
+        await ctx.send(f'`/find {update}`')
+        await asyncio.sleep(60)
+    await ctx.send('Finished updating.')
 
 def setup(bot):
     bot.add_listener(check, 'on_message')
