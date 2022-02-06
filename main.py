db3fdafecf79641f390c5b888d100459f8dc5c9f
Gary <gary.github@gmail.com>
2022-02-06 13:00:21 +0800

    Bugfix

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ import nexity
 if __name__ == '__main__':
     os.environ['CREDS'] = Path('creds').read_text()
     nexity.load_env()
-    nexity.load_local('exts.card')
+    nexity.load_local('quartz.card')
     nexity.run()
 
