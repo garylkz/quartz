f7efd2b21feeefc5a88cf4e31573dae25f9656d9
Gary <gary.github@gmail.com>
2022-02-22 20:37:47 +0800

    Update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import logging
-import os
-from pathlib import Path
-
-import nexity
-
-
 if __name__ == '__main__':
+    import logging
+    import os
+    from pathlib import Path
+
+    import nexity
+
+
     logging.basicConfig(level=logging.DEBUG)
-    os.environ['CREDS'] = Path('creds').read_text()
+    os.environ['CREDS'] = Path('creds.json').read_text()
+
     nexity.load_env()
     nexity.load_local('quartz.card')
     nexity.run()
-
