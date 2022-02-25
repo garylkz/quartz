b77f6899578a55e595b2f27132c1e3127104d6f8
Gary <gary.github@gmail.com>
2022-02-25 21:13:11 +0800

    Implement session break

--- quartz/cue.py
+++ quartz/cue.py
@@ -15,13 +15,17 @@ def get_card(code: str) -> dict:
 def get_updates(since: int) -> List[str]:
     return get(f'updateList/{since}')
 
-def get_card_updates(since: int) -> List[dict]:
+def get_card_updates(since: int, size: int = 500) -> List[dict]:
     session = requests.Session()
     g = lambda x : session.get(f'{API}/{x}').json()
     codes = g(f'updateList/{since}')
     logging.info(f'{len(codes)} card(s)')
     cards = []
     for i in range(len(codes)):
+        if i > 0 and i % size == 0:
+            session.close()
+            session = requests.Session()
         logging.info(f'{codes[i]}:{i}')
-        cards.append(g(f'detail/{codes[i]}'))
+        card = g(f'detail/{codes[i]}')
+        cards.append(card)
     return cards
