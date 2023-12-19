c3369e78954ae2c0917852695d010000439791aa
Gary Lim <37288203+garylkz@users.noreply.github.com>
2023-12-19 13:45:56 +0800

    Added validation for firstPull and modifiedDate

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = Path('requirements.txt').read_text().splitlines()
 
 setuptools.setup(
         name='quartz',
-        version='0.1.4',
+        version='0.1.5',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Sheets API & Discord API Automation',
